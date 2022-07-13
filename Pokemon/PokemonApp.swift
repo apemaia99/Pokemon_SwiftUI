8cc9e50ec869e902cd51051d6211a39098d47df3
Alessando Di Maio <92519597+apemaia99@users.noreply.github.com>
2022-07-13 20:18:15 +0200

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Alessandro Di Maio on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
